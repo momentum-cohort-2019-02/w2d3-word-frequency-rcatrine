327fb8af4bcab9880229699fa05662b2f3bafca2
Ryan Catrine <ryancatrine@gmail.com>
2019-02-21 14:17:31 -0500

    Added latest Word Frequency assignment.

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -5,9 +7,61 @@ STOP_WORDS = [
 ]
 
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+def normalize_text(text):
+    """
+    Given a text, lowercases it, removes all punctuation, 
+    and replaces all whitespace with normal spaces. Multiple whitespace will
+    be compressed into a single space.
+    """
+    text = text.casefold()
+    valid_chars = string.ascii_letters + string.whitespace + string.digits
+
+    # Remove all punctuation
+    new_text = ""
+    for char in text:
+        if char in valid_chars:
+            new_text += char
+
+    text = new_text
+    text = text.replace("\n", " ")
+    return text
+
+
+def print_word_freq(filename):
+    """Read in `filename` and print out the frequency of words in that file."""
+    with open(filename) as file:
+        text = file.read()
+
+    text = normalize_text(text)
+    words = []
+    for word in text.split(" "):
+        if word != '' and word not in STOP_WORDS:
+            words.append(word)
+
+# ________________________________________________________________
+    ## What now?
+    # Get a dictionary of word frequencies and print it out
+
+    # Imports seneca_falls.txt
+    import re
+    import string
+    word_freq = {}
+    document_text = open("seneca_falls.txt", "r")
+    text_string = document_text.read().lower()
+    match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
+    
+    for words in match_pattern:
+        count = word_freq.get(word,0)
+        # Reaches into dictionary, counts words
+        word_freq[word] = count + 1
+
+    word_freq_list = word_freq.keys()
+
+    for words in word_freq_list:
+        print (words, word_freq[words])
+
+# ________________________________________________________________
+  
 
 
 if __name__ == "__main__":
